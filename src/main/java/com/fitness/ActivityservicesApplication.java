634ff5bcb3cc58619cc01e9c90f81b2a62faab6d
kapilraj10 <kapilrajkc10@gmail.com>
2026-01-02 14:37:06 +0545

    first commit

---
+++ src/main/java/com/fitness/ActivityservicesApplication.java
@@ -0,0 +1,13 @@
+package com.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityservicesApplication.class, args);
+	}
+
+}
